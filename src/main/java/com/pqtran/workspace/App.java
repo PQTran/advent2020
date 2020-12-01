d3e7c0bd89ede6ee35786d1d636122923236e942
Paul Tran <ptranx23@gmail.com>
2020-12-01 05:40:27 -0800

    Initial commit

---
+++ src/main/java/com/pqtran/workspace/App.java
@@ -0,0 +1,12 @@
+package com.pqtran.workspace;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
